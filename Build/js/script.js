f8a4d3a8c1af9f804286f5adfd076fa7fb9ed267
howardpanton <howardpanton@gmail.com>
2013-06-30 20:50:17 +0100

    added twitter

--- Build/js/script.js
+++ Build/js/script.js
@@ -12,13 +12,17 @@ $("p.credit").click(function () {
   $(this).parents('.flip-container').addClass('flip');
 });
 
+$("p.close").click(function () {
+  $(this).parents('.flip-container').removeClass('flip');
+});
+
 $(".play-button").click(function () {
   var test = $(this).attr("data-video");
   var thisDiv = $('.content-video').find("[id='" + test +"']");
   thisDiv.siblings('.play-btn').hide();
   var Vimeo = thisDiv.next('.video');
   var videoID = Vimeo.attr("data-item");
-  var setHTML = "<iframe src=http://player.vimeo.com/video/" + videoID + "?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff&amp;autoplay=1\" width=\"1281\" height=\"720\" frameborder=\"0\" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> ";
+  var setHTML = "<iframe src=http://player.vimeo.com/video/" + videoID + "?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff&amp;autoplay=1\" width=\"1400\" height=\"787\" frameborder=\"0\" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> ";
   Vimeo.removeClass('is-hidden');
   Vimeo.children('.responsive-vid').html(setHTML);
   thisDiv.hide();
